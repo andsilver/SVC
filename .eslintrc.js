b7626eae44c762ce1545c84a830057565706022d
Eli <elicloudh@gmail.com>
2018-11-26 15:45:48 +0500

    Init npm with dev setup

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "rules": {
+      "linebreak-style": 0,
+      "no-multiple-empty-lines": 0,
+      "comma-dangle": 0,
+      "no-underscore-dangle": 0,
+      "no-unused-vars": 1,
+      "no-param-reassign": 0,
+      "camelcase": 0,
+      "eqeqeq": 0,
+      "no-console": 0,
+      "no-use-before-define": 0,
+      "no-unused-vars": 0,
+      "max-len": 0,
+      "no-plusplus": 0,
+      "no-sparse-arrays": 0,
+      "global-require": 0
+  }
+};
