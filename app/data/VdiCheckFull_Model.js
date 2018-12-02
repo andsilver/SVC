d8322c77ddf1f3b417c7838c0c1d7983fe3bfdde
Eli <elicloudh@gmail.com>
2018-12-03 01:27:26 +0500

    Add data

---
+++ app/data/VdiCheckFull_Model.js
@@ -0,0 +1,240 @@
+exports = {
+  Response: {
+    StatusCode: 'Success',
+    StatusMessage: 'Success',
+    StatusInformation: {},
+    MetaDataForKeys: [
+      {
+        Name: 'Vrm',
+        Type: 'String',
+        IsMandatory: true
+      }
+    ],
+    MetaDataForItems: {
+      CertificateOfDestructionIssued: {
+        Name: 'CertificateOfDestructionIssued',
+        Type: 'Boolean'
+      },
+      Colour: {
+        Name: 'Colour',
+        Type: 'String'
+      },
+      ColourChangeCount: {
+        Name: 'ColourChangeCount',
+        Type: 'Int32'
+      },
+      Data: {
+        Name: 'Data',
+        Type: 'String'
+      },
+      DateFirstRegistered: {
+        Name: 'DateFirstRegistered',
+        Type: 'String'
+      },
+      DateFirstRegisteredUk: {
+        Name: 'DateFirstRegisteredUk',
+        Type: 'String'
+      },
+      EngineCapacity: {
+        Name: 'EngineCapacity',
+        Type: 'Int32'
+      },
+      ExportDate: {
+        Name: 'ExportDate',
+        Type: 'String'
+      },
+      Exported: {
+        Name: 'Exported',
+        Type: 'Boolean'
+      },
+      FinanceRecordCount: {
+        Name: 'FinanceRecordCount',
+        Type: 'Int32'
+      },
+      FinanceRecordList: {
+        Name: 'FinanceRecordList',
+        Type: 'List'
+      },
+      FuelType: {
+        Name: 'FuelType',
+        Type: 'String'
+      },
+      GearCount: {
+        Name: 'GearCount',
+        Type: 'Int32'
+      },
+      HighRiskRecordCount: {
+        Name: 'HighRiskRecordCount',
+        Type: 'Int32'
+      },
+      HighRiskRecordList: {
+        Name: 'HighRiskRecordList',
+        Type: 'List'
+      },
+      ImportDate: {
+        Name: 'ImportDate',
+        Type: 'String'
+      },
+      Imported: {
+        Name: 'Imported',
+        Type: 'Boolean'
+      },
+      ImportedFromOutsideEu: {
+        Name: 'ImportedFromOutsideEu',
+        Type: 'Boolean'
+      },
+      ImportUsedBeforeUkRegistration: {
+        Name: 'ImportUsedBeforeUkRegistration',
+        Type: 'Boolean'
+      },
+      LatestColourChangeDate: {
+        Name: 'LatestColourChangeDate',
+        Type: 'String'
+      },
+      LatestKeeperChangeDate: {
+        Name: 'LatestKeeperChangeDate',
+        Type: 'String'
+      },
+      LatestV5cIssuedDate: {
+        Name: 'LatestV5cIssuedDate',
+        Type: 'String'
+      },
+      LookupStatusCode: {
+        Name: 'LookupStatusCode',
+        Type: 'String'
+      },
+      LookupStatusMessage: {
+        Name: 'LookupStatusMessage',
+        Type: 'String'
+      },
+      Make: {
+        Name: 'Make',
+        Type: 'String'
+      },
+      MileageAnomalyDetected: {
+        Name: 'MileageAnomalyDetected',
+        Type: 'Boolean'
+      },
+      MileageRecordCount: {
+        Name: 'MileageRecordCount',
+        Type: 'Int32'
+      },
+      MileageRecordList: {
+        Name: 'MileageRecordList',
+        Type: 'List'
+      },
+      Model: {
+        Name: 'Model',
+        Type: 'String'
+      },
+      PlateChangeCount: {
+        Name: 'PlateChangeCount',
+        Type: 'Int32'
+      },
+      PlateChangeList: {
+        Name: 'PlateChangeList',
+        Type: 'List'
+      },
+      PreviousColour: {
+        Name: 'PreviousColour',
+        Type: 'String'
+      },
+      PreviousKeeperCount: {
+        Name: 'PreviousKeeperCount',
+        Type: 'Int32'
+      },
+      PreviousKeepers: {
+        Name: 'PreviousKeepers',
+        Type: 'String'
+      },
+      ScrapDate: {
+        Name: 'ScrapDate',
+        Type: 'String'
+      },
+      Scrapped: {
+        Name: 'Scrapped',
+        Type: 'Boolean'
+      },
+      Stolen: {
+        Name: 'Stolen',
+        Type: 'Boolean'
+      },
+      StolenContactNumber: {
+        Name: 'StolenContactNumber',
+        Type: 'String'
+      },
+      StolenDate: {
+        Name: 'StolenDate',
+        Type: 'String'
+      },
+      StolenInfoSource: {
+        Name: 'StolenInfoSource',
+        Type: 'String'
+      },
+      StolenMiaftrRecordCount: {
+        Name: 'StolenMiaftrRecordCount',
+        Type: 'Int32'
+      },
+      StolenMiaftrRecordList: {
+        Name: 'StolenMiaftrRecordList',
+        Type: 'String'
+      },
+      StolenPoliceForce: {
+        Name: 'StolenPoliceForce',
+        Type: 'String'
+      },
+      StolenStatus: {
+        Name: 'StolenStatus',
+        Type: 'String'
+      },
+      TransmissionType: {
+        Name: 'TransmissionType',
+        Type: 'String'
+      },
+      VicTestDate: {
+        Name: 'VicTestDate',
+        Type: 'String'
+      },
+      VicTested: {
+        Name: 'VicTested',
+        Type: 'Boolean'
+      },
+      VicTestResult: {
+        Name: 'VicTestResult',
+        Type: 'String'
+      },
+      VinLast5: {
+        Name: 'VinLast5',
+        Type: 'String'
+      },
+      Vrm: {
+        Name: 'Vrm',
+        Type: 'String'
+      },
+      WriteOffCategory: {
+        Name: 'WriteOffCategory',
+        Type: 'String'
+      },
+      WriteOffDate: {
+        Name: 'WriteOffDate',
+        Type: 'String'
+      },
+      WriteOffRecordCount: {
+        Name: 'WriteOffRecordCount',
+        Type: 'Int32'
+      },
+      WriteOffRecordList: {
+        Name: 'WriteOffRecordList',
+        Type: 'String'
+      },
+      WrittenOff: {
+        Name: 'WrittenOff',
+        Type: 'Boolean'
+      },
+      YearOfManufacture: {
+        Name: 'YearOfManufacture',
+        Type: 'String'
+      }
+    }
+  }
+};
---
+++ app/data/VdiCheckFull_Success.js
@@ -0,0 +1,102 @@
+exports = {
+  Response: {
+    StatusCode: 'Success',
+    StatusMessage: 'Success',
+    StatusInformation: {
+      Lookup: {
+        StatusCode: 'Success',
+        StatusMessage: 'Success',
+        AdviceTextList: []
+      }
+    },
+    DataItems: {
+      DateFirstRegisteredUk: '15/06/2012',
+      WriteOffDate: null,
+      Make: 'VOLKSWAGEN',
+      EngineCapacity: 1968,
+      FinanceRecordList: [],
+      WrittenOff: false,
+      VicTestDate: null,
+      WriteOffRecordList: null,
+      CertificateOfDestructionIssued: null,
+      TransmissionType: 'Manual',
+      PreviousKeepers: null,
+      LatestKeeperChangeDate: '08/01/2013',
+      PreviousColour: null,
+      Model: 'SHARAN SEL BLUE TECH TDI',
+      PlateChangeList: null,
+      Vrm: 'KM12AKK',
+      StolenContactNumber: null,
+      ScrapDate: null,
+      ImportedFromOutsideEu: false,
+      ColourChangeCount: 0,
+      VicTested: null,
+      Imported: false,
+      StolenPoliceForce: null,
+      LookupStatusCode: null,
+      StolenMiaftrRecordList: null,
+      GearCount: 6,
+      LatestV5cIssuedDate: '15/06/2012',
+      StolenInfoSource: null,
+      FuelType: 'DIESEL',
+      StolenDate: null,
+      WriteOffRecordCount: 0,
+      LatestColourChangeDate: null,
+      MileageRecordList: [
+        {
+          DateOfInformation: '27/11/2018',
+          SourceOfInformation: 'MOT',
+          Mileage: 90553
+        },
+        {
+          DateOfInformation: '23/11/2018',
+          SourceOfInformation: 'MOT',
+          Mileage: 90552
+        },
+        {
+          DateOfInformation: '08/11/2017',
+          SourceOfInformation: 'MOT',
+          Mileage: 76633
+        },
+        {
+          DateOfInformation: '15/11/2016',
+          SourceOfInformation: 'MOT',
+          Mileage: 64047
+        },
+        {
+          DateOfInformation: '21/10/2015',
+          SourceOfInformation: 'MOT',
+          Mileage: 50500
+        },
+        {
+          DateOfInformation: '21/10/2015',
+          SourceOfInformation: 'MOT',
+          Mileage: 50500
+        }
+      ],
+      ImportUsedBeforeUkRegistration: null,
+      MileageAnomalyDetected: false,
+      StolenStatus: null,
+      StolenMiaftrRecordCount: 0,
+      MileageRecordCount: 6,
+      LookupStatusMessage: null,
+      Scrapped: false,
+      HighRiskRecordCount: 0,
+      Stolen: false,
+      ImportDate: null,
+      Exported: false,
+      HighRiskRecordList: null,
+      WriteOffCategory: null,
+      ExportDate: null,
+      VinLast5: '46227',
+      VicTestResult: null,
+      Data: null,
+      DateFirstRegistered: '15/06/2012',
+      Colour: 'GREY',
+      PlateChangeCount: 0,
+      PreviousKeeperCount: 1,
+      FinanceRecordCount: 0,
+      YearOfManufacture: '2012'
+    }
+  }
+};
