b08e1b3cf1de09340704bfa68af4565bbcc3df81
Eli <elicloudh@gmail.com>
2018-12-13 04:14:38 +0500

    Add dataPackages

---
+++ app/data/dataPackages.js
@@ -0,0 +1,16 @@
+module.exports = [
+  'BatteryData',
+  'MotHistoryAndTaxStatusData',
+  'MotHistoryData',
+  'PostcodeLookup',
+  'SpecAndOptionsData',
+  'TyreData',
+  'ValuationCanPrice',
+  'ValuationData',
+  'VdiCheckFull',
+  'VehicleAndMotHistory',
+  'VehicleData',
+  'VehicleDataIRL',
+  'VehicleImageData',
+  'VehicleTaxData'
+].toString().toLowerCase();
